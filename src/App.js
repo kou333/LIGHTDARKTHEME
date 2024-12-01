93d3c43fa026b6ba4dc8f08aa633e44822de3038
kou333 <naikkoustubh@gmail.com>
2024-12-01 15:31:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LightDarkMode from './components/light-dark-mode';
+
+function App() {
+  return (
+    <div className="App">
+      <LightDarkMode/>
+    </div>
+  );
+}
+
+export default App;
